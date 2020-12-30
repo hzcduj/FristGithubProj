01a5cfd9fd1e27dd296d38478e4a054f2eab8a49
hzcduj <43994081+hzcduj@users.noreply.github.com>
2020-12-30 15:01:57 +0800

    Delete FristGithub.java

--- FristGithub.java
+++
@@ -1,9 +0,0 @@
-package cn.cvs.controller;
-
-public class FristGithub {
-    public static void main(String[] args) {
-        System.out.println(
-                "this is my frist github again"
-        );
-    }
-}
