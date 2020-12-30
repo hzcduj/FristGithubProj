4f9bbbe0cff49276677c321af6e07cc0a07171d3
hzcduj <43994081+hzcduj@users.noreply.github.com>
2020-12-30 14:50:00 +0800

    Add files via upload

---
+++ FristGithub.java
@@ -0,0 +1,9 @@
+package cn.cvs.controller;
+
+public class FristGithub {
+    public static void main(String[] args) {
+        System.out.println(
+                "this is my frist github"
+        );
+    }
+}
